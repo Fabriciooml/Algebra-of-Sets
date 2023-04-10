ad61b537afffb494540562cb10755d433f0a2837
Fabrício Lima <fabriciooml@gmail.com>
2023-04-10 09:18:47 -0300

    fix: remove wrong code

--- src/main.rs
+++ src/main.rs
@@ -3,21 +3,28 @@ use std::fs::File;
 use std::io::{BufRead, BufReader, stdin};
 
 fn read_sets_from_file() -> Vec<IndexSet<i32>> {
-  let mut sets_vector: Vec<IndexSet<i32>> = Vec::with_capacity(1024);
+  let mut sets_vector: Vec<IndexSet<i32>> = Vec::new();
   let file = File::open("src/input.txt").expect("Failed to open file");
-  sets_vector.extend(BufReader::new(file).lines().map(read_set));
+  
+  for line in BufReader::new(file).lines() {
+      sets_vector.push(read_set(line));
+  }
   sets_vector
 }
 
-fn read_set(line: &str) -> IndexSet<i32> {
+fn read_set(line: Result<String, std::io::Error>) -> IndexSet<i32> {
   let mut set = IndexSet::new();    
 
-  let string = line.replace(&['{', '}'][..], "");
-  let tokens: Vec<&str> = string.split_whitespace().collect();
-  for i in 2..tokens.len() {
-    let values = tokens[i].split(',').map(|s| s.trim().parse::<i32>()).filter_map(Result::ok);
-    set.extend(values);
-}
+  let string = line.expect("Failed to read line");
+      let string = string.replace(&['{', '}'][..], "");
+      let tokens: Vec<&str> = string.split_whitespace().collect();
+      for i in 2..tokens.len(){
+          for value in tokens[i].split(',') {
+              if let Ok(num) = value.trim().parse::<i32>() {
+                  set.insert(num);
+              }
+          }
+      } 
   set
 }
 
